8605eb530b4610b8f7fbf5192733de4a2b7de2ed
theepthy <evergoldhariny2007@gmail.com>
2024-11-08 18:54:02 +0530

    motorbike component is modified

--- bikeshowroom/src/Components/MotorBikeContainer/MotorBikeContainer.tsx
+++ bikeshowroom/src/Components/MotorBikeContainer/MotorBikeContainer.tsx
@@ -1,27 +1,65 @@
-import React from 'react'
+// import React from 'react'
+
+// const MotorBikeContainer = () => {
+//   return (
+//     <main>
+//       <h1 className='uppercase text-center text-4xl font-bold bg-gray-800 text-white hover:shadow-lg
+//        hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer animate-fadeIn'>
+//        Motor Bike
+// </h1>
+//     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+   
+ 
+//       <div className="p-4 mx-auto">
+//         <img src='https://static.wixstatic.com/media/c1981a_04ac7b3356f0428f839fd1cbfc039a13~mv2.png/v1/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/c1981a_04ac7b3356f0428f839fd1cbfc039a13~mv2.png' />
+//       </div>
+//       <div className="p-4 mx-auto">
+//         <img src='https://static.wixstatic.com/media/c1981a_d12aecb71fb54ee98488f95c1be71778~mv2.png/v1/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/c1981a_d12aecb71fb54ee98488f95c1be71778~mv2.png' />
+//       </div>
+//       <div className="p-4 mx-auto">
+//         <img src='https://static.wixstatic.com/media/c1981a_e18653f60ecd4ae995a5644b32e285bb~mv2.png/v1/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/c1981a_e18653f60ecd4ae995a5644b32e285bb~mv2.png' />
+//       </div>
+//     </div>
+//     </main>
+//   )
+// }
+
+// export default MotorBikeContainer
+import React from 'react';
 
 const MotorBikeContainer = () => {
   return (
     <main>
-      <h1 className='uppercase text-center text-4xl font-bold bg-gray-800 text-white hover:shadow-lg
-       hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer animate-fadeIn'>
-       Motor Bike
-</h1>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-   
- 
-      <div className="p-4 mx-auto">
-        <img src='https://static.wixstatic.com/media/c1981a_04ac7b3356f0428f839fd1cbfc039a13~mv2.png/v1/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/c1981a_04ac7b3356f0428f839fd1cbfc039a13~mv2.png' />
+      <h1 className="uppercase text-center text-4xl font-bold bg-gray-800 text-white hover:shadow-lg
+        hover:scale-105 transition-all duration-300 ease-in-out cursor-pointer animate-fadeIn">
+        Motor Bike
+      </h1>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 perspective-1000">
+        <div className="p-4 mx-auto animate-rotate-3d">
+          <img
+            src="https://static.wixstatic.com/media/c1981a_04ac7b3356f0428f839fd1cbfc039a13~mv2.png/v1/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/c1981a_04ac7b3356f0428f839fd1cbfc039a13~mv2.png"
+            alt="Motorbike 1"
+            className="transform transition-transform duration-300"
+          />
+        </div>
+        <div className="p-4 mx-auto animate-rotate-3d">
+          <img
+            src="https://static.wixstatic.com/media/c1981a_d12aecb71fb54ee98488f95c1be71778~mv2.png/v1/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/c1981a_d12aecb71fb54ee98488f95c1be71778~mv2.png"
+            alt="Motorbike 2"
+            className="transform transition-transform duration-300"
+          />
+        </div>
+        <div className="p-4 mx-auto animate-rotate-3d">
+          <img
+            src="https://static.wixstatic.com/media/c1981a_e18653f60ecd4ae995a5644b32e285bb~mv2.png/v1/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/c1981a_e18653f60ecd4ae995a5644b32e285bb~mv2.png"
+            alt="Motorbike 3"
+            className="transform transition-transform duration-300"
+          />
+        </div>
       </div>
-      <div className="p-4 mx-auto">
-        <img src='https://static.wixstatic.com/media/c1981a_d12aecb71fb54ee98488f95c1be71778~mv2.png/v1/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/c1981a_d12aecb71fb54ee98488f95c1be71778~mv2.png' />
-      </div>
-      <div className="p-4 mx-auto">
-        <img src='https://static.wixstatic.com/media/c1981a_e18653f60ecd4ae995a5644b32e285bb~mv2.png/v1/fill/w_314,h_314,al_c,q_80,usm_0.66_1.00_0.01/c1981a_e18653f60ecd4ae995a5644b32e285bb~mv2.png' />
-      </div>
-    </div>
     </main>
-  )
-}
+  );
+};
 
-export default MotorBikeContainer
+export default MotorBikeContainer;

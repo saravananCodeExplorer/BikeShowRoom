94eb874689b5bb8350b5db29055a7e4d202943db
theepthy <evergoldhariny2007@gmail.com>
2024-11-11 14:16:15 +0530

    about page modified

--- bikeshowroom/src/Components/About/About.tsx
+++ bikeshowroom/src/Components/About/About.tsx
@@ -1,14 +1,26 @@
+
+
 import React from 'react'
 
 const About = () => {
   return (
-    <div className=''>
-      <div className="bg-white md:w-[100vh] mx-auto px-16 pt-6">
-        <h1 className='text-center text-2xl text-black'>ABOUT GUNA MOTORS</h1>
-        <p className='text-center pt-6 text-black'>This is the Official Website of GUNA MOTORS YAMAHA. We are a Motorcycle Main dealer for Yamaha in Madurai. And also the official host for the BLUE STREAKS MADURAI, an Official Motorcycle Riding Club for Yamaha Motorbike owners in Madurai.</p>
-        <p className='text-center mt-5 pb-6 text-black'>We are passionate people working towards sharing our riding passion with our customers and help them have a better experience while riding our bikes on the road for all kinds of purposes. We assist customers not only in buying the vehicle, but in after sales value added services too. We have knowledgeable manpower’s who can guide you in the different categories of Motorcycling, like Touring, Travelling and Motorsports Racing. We are waiting to serve you better. Join us to share the passion for the motorcycle and experience the best customer service ever. Come Let’s REV YOUR HEART!!!!</p>
+
+      <div className="bg-white shadow-lg hover:shadow-2xl transition-shadow duration-300 rounded-lg overflow-hidden md:w-[100vh] lg:w-[100vh] mx-auto p-8 transform hover:scale-105">
+        <h1 className='text-center text-3xl font-bold text-indigo-700'>ABOUT GUNA MOTORS</h1>
+        
+        <p className='text-center pt-6 text-gray-700 leading-relaxed'>
+          This is the Official Website of GUNA MOTORS YAMAHA. We are a Motorcycle Main dealer for Yamaha in Madurai. And also the official host for the BLUE STREAKS MADURAI, an Official Motorcycle Riding Club for Yamaha Motorbike owners in Madurai.
+        </p>
+        
+        <p className='text-center mt-5 pb-6 text-gray-700 leading-relaxed'>
+          We are passionate people working towards sharing our riding passion with our customers and help them have a better experience while riding our bikes on the road for all kinds of purposes. We assist customers not only in buying the vehicle, but in after-sales value-added services too.
+        </p>
+
+        <p className='text-center mt-5 pb-6 text-gray-700 leading-relaxed'>
+          Our knowledgeable team can guide you in different categories of motorcycling, like Touring, Travelling, and Motorsports Racing. We’re ready to serve you! Join us to share the passion for motorcycles and experience unparalleled customer service. Come, Let’s REV YOUR HEART!
+        </p>
       </div>
-    </div>
+  
   )
 }
 
